677d5e6ee7be0ad0a54510e5a55da66b387e99e2
986299679@qq.com <986299679@qq.com>
2020-07-08 16:57:52 +0800

    change tests.

--- test.lua
+++ test.lua
@@ -5,14 +5,9 @@ local rc4hex = require "rc4hex"
 local data = "522252147"
 local key = "VMI9yw0eTtQ0z14l"
 local en_string = rc4hex.en_hex(data, key)
---print(en_string)
-freeswitch.consoleLog("info", en_string)
+print(en_string)
+--freeswitch.consoleLog("info", en_string)
 
 ----en_string = "a3396500a76913e34b"
 --data = rc4hex.de_hex(en_string, key)
 --print(data)
-
----- time_test.lua
---local mylualib = require "mylualib"
---local now = mylualib.milli_time()
---print(os.date("%Y-%m-%d %H:%M:%S").. ":millisecond = " .. now)
